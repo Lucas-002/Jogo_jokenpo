bbef153474e010d9c199d0ec4621a2be708dc9af
Lucas Márcio <65137541+Lucas-002@users.noreply.github.com>
2024-07-25 21:30:13 -0300

    Update jokenpo.py
    Melhorias Implementadas:
    
    1. Validação de Entrada: Adicionado um loop `while` para garantir que a entrada do jogador seja válida (0, 1 ou 2).
    2. Formatação: Simplifiquei a impressão da jogada do computador e do jogador usando f-strings para melhor legibilidade.
    3. Eliminação de Redundâncias: Criei uma matriz `resultados` para armazenar todos os possíveis resultados, eliminando a necessidade de várias instruções `if-elif`.
    4. Comentários: Adicionei comentários para explicar cada seção do código.

--- jokenpo.py
+++ jokenpo.py
@@ -1,46 +1,50 @@
 from random import randint
 from time import sleep
 
+# Definindo os itens do jogo
 itens = ['pedra', 'papel', 'tesoura']
 computador = randint(0, 2)
 
-#INDICE
-print('{:-^40}'.format(' Welcome or Game '))
-print("""(0) PEDRA
+# Indice
+print('{:-^40}'.format(' Welcome to the Game '))
+print("""Escolha sua jogada:
+(0) PEDRA
 (1) PAPEL
 (2) TESOURA""")
-jogador = int(input('Qual é a sua opção: '))
 
-print('\033[32mJO'), sleep(1)
-print('\033[33mKEN'), sleep(1)
-print('\033[36mPOOO'), sleep(1)
+# Verificação de entrada do jogador
+while True:
+    try:
+        jogador = int(input('Qual é a sua opção: '))
+        if jogador in [0, 1, 2]:
+            break
+        else:
+            print("Opção inválida! Por favor, escolha 0, 1 ou 2.")
+    except ValueError:
+        print("Entrada inválida! Por favor, insira um número (0, 1 ou 2).")
 
-#LOGICA DO JOGO
+# Contagem regressiva
+print('\033[32mJO')
+sleep(1)
+print('\033[33mKEN')
+sleep(1)
+print('\033[36mPOOO')
+sleep(1)
+
+# Lógica do jogo
 print('\033[35m')
 print('-==-==' * 4)
-print('Computador jogou {}'.format(itens[computador]))
-print('Você jogou {}'.format(itens[jogador]))
+print(f'Computador jogou {itens[computador]}')
+print(f'Você jogou {itens[jogador]}')
 print('-==-==' * 4)
 print('\033[m')
 
-if computador == 0:  # PEDRA
-    if jogador == 0:
-        print('\033[33mIMPATE!')
-    elif jogador == 1:
-        print('\033[34mVOCÊ GANHOU!!!')
-    elif jogador == 2:
-        print('\033[31mVOCÊ PERDEU!!!')
-elif computador == 1:  # PAPEL
-    if jogador == 0:
-        print('\033[31mVOCÊ PERDEU!!!')
-    elif jogador == 1:
-        print('\033[33mIMPATE!')
-    elif jogador == 2:
-        print('\033[34mVOCÊ GANHOU!!!')
-elif computador == 2:  # TESOURA
-    if jogador == 0:
-        print('\033[34mVOCÊ GANHOU!!!')
-    elif jogador == 1:
-        print('\033[31mVOCÊ  PERDEU!!!')
-    elif jogador == 2:
-        print('\033[33mIMPATE!')
+# Resultados possíveis
+resultados = [
+    ['\033[33mEMPATE!', '\033[34mVOCÊ GANHOU!!!', '\033[31mVOCÊ PERDEU!!!'],
+    ['\033[31mVOCÊ PERDEU!!!', '\033[33mEMPATE!', '\033[34mVOCÊ GANHOU!!!'],
+    ['\033[34mVOCÊ GANHOU!!!', '\033[31mVOCÊ PERDEU!!!', '\033[33mEMPATE!']
+]
+
+# Imprimindo resultado
+print(resultados[computador][jogador])
